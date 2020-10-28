3aea377badaafd6655ed6b91e1a8b86356a5d0c9
naston70 <naston70@icloud.com>
2020-10-29 00:25:13 +0100

    completed pcost.py exercise

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,19 @@
 # pcost.py
-#
+# Opens portfolio.csv, reads all lines and calculates total to purchase all of the shares
 # Exercise 1.27
+
+
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f).split(',')
+total = 0
+for line in f:
+	row = line.split(',')
+	total += int(row[1]) * float(row[2].strip('\n'))
+f.close()
+
+print(f'Total cost of portfolio: {total}')
+
+
+# Expected output:
+# ➜  Work git:(main) ✗ python3 pcost.py
+#    Total cost of portfolio: 44671.15
